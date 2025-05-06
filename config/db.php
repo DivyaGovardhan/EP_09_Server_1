57c676bc0b76830a93ce086ef00464ce56c9db63
DivyaGovardhan <aslikyan2006@gmail.com>
2025-05-06 09:14:28 +0700

    registration

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'xksenned_m5',
+    'username' => 'xksenned',
+    'password' => 'RhDkgu',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
