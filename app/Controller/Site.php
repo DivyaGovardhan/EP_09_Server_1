a5b3f3cf70a160cb76a82867e41698d51fb2030d
DivyaGovardhan <aslikyan2006@gmail.com>
2025-09-07 20:34:51 +0700

    middlewares

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -7,9 +7,43 @@ use Src\View;
 use Src\Request;
 use Model\User;
 use Src\Auth\Auth;
+use Src\Validator\Validator;
 
 class Site
 {
+    public function start(): void
+    {
+       // Fetch method and URI from somewhere
+       $httpMethod = $_SERVER['REQUEST_METHOD'];
+       $uri = $_SERVER['REQUEST_URI'];
+
+       // Strip query string (?foo=bar) and decode URI
+       if (false !== $pos = strpos($uri, '?')) {
+           $uri = substr($uri, 0, $pos);
+       }
+       $uri = rawurldecode($uri);
+       $uri = substr($uri, strlen($this->prefix));
+
+       $dispatcher = new Dispatcher($this->routeCollector->getData());
+
+       $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+       switch ($routeInfo[0]) {
+           case Dispatcher::NOT_FOUND:
+               throw new Error('NOT_FOUND');
+           case Dispatcher::METHOD_NOT_ALLOWED:
+               throw new Error('METHOD_NOT_ALLOWED');
+           case Dispatcher::FOUND:
+               $handler = $routeInfo[1];
+               $vars = array_values($routeInfo[2]);
+    //Вызываем обработку всех Middleware
+               $vars[] = Middleware::single()->go($httpMethod, $uri, new Request());
+               $class = $handler[0];
+               $action = $handler[1];
+               call_user_func([new $class, $action], ...$vars);
+               break;
+       }
+    }
+
     public function index(Request $request): string
     {
         $posts = Post::where('id', $request->id)->get();
@@ -23,11 +57,29 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method === 'POST' && User::create($request->all())) {
-            app()->route->redirect('/hello');
-        }
-        return new View('site.signup');
+       if ($request->method === 'POST') {
+
+           $validator = new Validator($request->all(), [
+               'name' => ['required'],
+               'login' => ['required', 'unique:users,login'],
+               'password' => ['required']
+           ], [
+               'required' => 'Поле :field пусто',
+               'unique' => 'Поле :field должно быть уникально'
+           ]);
+
+           if($validator->fails()){
+               return new View('site.signup',
+                   ['message' => json_encode($validator->errors(), JSON_UNESCAPED_UNICODE)]);
+           }
+
+           if (User::create($request->all())) {
+               app()->route->redirect('/login');
+           }
+       }
+       return new View('site.signup');
     }
+
     public function login(Request $request): string
     {
         //Если просто обращение к странице, то отобразить форму
