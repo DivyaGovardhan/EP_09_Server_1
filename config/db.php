0c3144ea880d58745cd276d1ea0eccb1820dc4b1
DivyaGovardhan <aslikyan2006@gmail.com>
2025-09-07 20:09:59 +0700

    user roles

--- config/db.php
+++ config/db.php
@@ -1,10 +1,21 @@
 <?php
+// return [
+//     'driver' => 'mysql',
+//     'host' => 'localhost',
+//     'database' => 'xksenned_m5',
+//     'username' => 'xksenned',
+//     'password' => 'RhDkgu',
+//     'charset' => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix' => '',
+// ];
+
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
     'database' => 'xksenned_m5',
-    'username' => 'xksenned',
-    'password' => 'RhDkgu',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
